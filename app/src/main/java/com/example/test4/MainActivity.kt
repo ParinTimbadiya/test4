e643efd8ba5cc2401f749a3ca7bce7afa41b05b0
ParinTimbadiya <parintimbadiya9603@gmail.com>
2022-12-16 12:39:33 +0530

    add comment

--- app/src/main/java/com/example/test4/MainActivity.kt
+++ app/src/main/java/com/example/test4/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 //    private lateinit var img_logo :ImageView
+//    private lateinit var img_logo :ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
